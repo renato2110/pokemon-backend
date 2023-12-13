a1bb72c6f8ef94c814b1d24ff2bfdef6de67764c
Renato Mainieri <renato.mainieri2110@gmail.com>
2023-12-12 23:03:51 -0600

    Add gym connection test

---
+++ src/action-tests/gym-connection-testing.test.ts
@@ -0,0 +1,18 @@
+// exploratory-testing.test.ts
+
+import { expect } from 'chai';
+import axios from 'axios';
+import { infoPath } from '../services/api-service';
+
+describe('Gym Connectivity Test', function () {
+    it('should successfully connect to the Gym endpoint', () => {
+        axios.get(infoPath)
+          .then((response) => {
+            expect(response.status).to.equal(200);
+          })
+          .catch((error) => {
+            throw error;
+          });
+      });
+      
+});
